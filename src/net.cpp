07e05f3744167b85691ebfa9978218722f68b41c
Leandro <nimbuscoin>
2015-02-12 16:06:09 -0200

    Fix on the seeder
    fixed

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"html.alltheco.in", "html.alltheco.in"},
+    {"alltheco.in", "html.alltheco.in"},
 	};
 
 void ThreadDNSAddressSeed(void* parg)
